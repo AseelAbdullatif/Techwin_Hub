791de98f7b1170b6501a6c333d18de51d4931187
LubabahSa <lubabah.sa@gmail.com>
2026-02-02 13:42:09 +0300

    chore: initialize React Native project with base navigation and auth screens

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { Text, View } from 'react-native';
+import MainStack from './src/stacks/MainStack';
+
+function App() {
+
+  return (
+    <MainStack />
+  );
+}
+
+
+
+
+
+export default App;
+
