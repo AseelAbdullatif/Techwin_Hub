f10cf41e7fda3c2054412eafbc13b1ccb90180a0
AseelAbdullatif <abdulatifaseel90@gmail.com>
2026-02-03 15:42:44 +0300

    add som file and ubdate others

--- App.js
+++ App.js
@@ -1,23 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  */
 
-import { Text, View } from 'react-native';
+// import { Text, View } from 'react-native';
+// import MainStack from './src/stacks/MainStack';
+
+// function App() {
+
+//   return (
+//     <MainStack />
+//   );
+// }
+
+
+
+
+
+// export default App;
+
+import React, { useEffect } from 'react';
+import { I18nManager, View, StatusBar } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import RNRestart from 'react-native-restart';
+import { isAndroid, storageHandler } from './src/utils/helpers/Helpers';
 import MainStack from './src/stacks/MainStack';
+// import { useColors } from './src/hooks/useColors';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-function App() {
 
-  return (
-    <MainStack />
-  );
+export default function App() {
+    const { i18n } = useTranslation();
+    const language = i18n.language;
+    //   const Colors = useColors();
+
+    useEffect(() => {
+        (async () => {
+            const lang = await storageHandler('get', 'language');
+            // const appLanguage = lang || language;
+            I18nManager.allowRTL(true);
+            I18nManager.forceRTL(true);
+            i18n.changeLanguage('ar');
+            storageHandler('store', 'language', 'ar');
+            if (isAndroid() && !I18nManager.isRTL) RNRestart.restart();
+        })();
+    }, []);
+
+    useEffect(() => {
+        I18nManager.allowRTL(language === 'ar');
+        I18nManager.forceRTL(language === 'ar');
+    }, [language]);
+
+    return (
+
+        <MainStack />
+
+    );
 }
-
-
-
-
-
-export default App;
-
---
+++ src/utils/colors/Color.js
@@ -0,0 +1,30 @@
+
+
+
+
+const Color = () => {
+  return (
+{
+BackgroundBlack:'#161616',
+LightBlack:'#232323',
+DarkGrey:'#3B3B3B',
+Grey:'#747474',
+LightGrey:'#DFDFDF',
+White:'#ffffff',
+LightBlue:'#B1EBF1',
+NormalRed:'#ff4500',
+NormalYellow:'#F2C200', 
+NormalPurple:'#7A3BF1',
+NormalBlue:'#03BFD3',
+LightGreen:'#0CC414',
+gradientBlueFir:'#55AEFB',
+gradientBlueSkySec:'#03BFD3',
+}
+
+  );};
+export default Color;
+
+// use 
+// import Color from './src/utils/colors/Color'
+// then:
+// Color.LightBlack
